5bb56b7c01cc17e3f80df6d79094691913a54eed
jiaq-li <lijiaqi.work@outlook.com>
2022-05-22 10:12:01 +0800

    跳转优化（最终版！）

--- www/js/upvideo_test.js
+++ www/js/upvideo_test.js
@@ -1,4 +1,3 @@
-<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.js"></script>
 var upcheck;
 // hTML5实现表单内的上传文件框，上传前预览视频，刷新预览video，使用HTML5 的File API,
 // 建立一个可存取到该file的url，一个空的video标签，ID为video0,把选择的文件显示在video标签中，实现视频预览功能。
@@ -43,3 +42,28 @@ function toret(){
 }
 
 
+// function upvideochange(){
+//     alert("ok");
+//     var objUrl = getObjectURL(this.files[0]);
+//     console.log("objUrl = " + objUrl);
+//     if (objUrl) {
+//         $("#video2up").attr("src", objUrl);
+//     }
+//     var  filePath=$( this ).val();
+//     if (filePath.indexOf( "mp4" )!=-1 || filePath.indexOf( "png" )!=-1){
+//         //修改允许类型
+//         $( ".fileerrorTip1" ).html( "" ).hide();
+//         var  arr=filePath.split( '\\' );
+//         var  fileName=arr[arr.length-1];
+//         $( ".showFileName1" ).html(fileName);
+//         alert("上传成功！关闭后跳转")
+//         window.location.href='result_compare.html'
+//         upcheck=true;
+//     } else {
+//         $( ".showFileName1" ).html( "" );
+//         $( ".fileerrorTip1" ).html( "您未上传文件，或者您上传文件类型有误！" ).show();
+//         upcheck=false;
+//         alert("未选择视频或类型有误，上传失败！")
+//         upcheck=false;
+//     }
+// }
